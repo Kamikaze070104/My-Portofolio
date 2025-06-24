35ede1a9067b07c04bad226ee88172c61bf01c50
Kamikaze070104 <faizalazzriel@upi.edu>
2025-06-24 20:41:49 +0700

    add new navbar components on folder components/Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold">Welcome to the React App</h1>
+    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc auctor ex ut eleifend dignissim. Donec dictum lectus accumsan quam imperdiet sollicitudin. Quisque fringilla urna eget vehicula viverra. Suspendisse venenatis ipsum id mauris feugiat consectetur. Aenean at dolor facilisis, aliquam odio feugiat, sagittis felis. In hac habitasse platea dictumst. Fusce in nunc vitae augue efficitur sollicitudin. Nulla a arcu risus. Vivamus elementum orci et justo accumsan accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur tortor leo, interdum ut convallis nec, interdum a quam. Ut eget erat non mi viverra bibendum. Donec a nulla non massa aliquet pulvinar id sed dolor. Cras imperdiet, lorem sit amet pharetra hendrerit, eros dui feugiat nibh, vitae suscipit dui augue congue turpis. Nullam mollis, ante id placerat feugiat, lorem dolor rhoncus diam, et pretium nisl neque sit amet tellus. Phasellus finibus porta tortor.
+
+Phasellus vulputate ligula a faucibus venenatis. Praesent sapien erat, consectetur vel pellentesque quis, imperdiet et turpis. Pellentesque volutpat augue ac finibus condimentum. Quisque dapibus vitae nibh vel faucibus. Integer sed ligula id nisi laoreet tincidunt consectetur eu nisl. Proin nec lobortis quam. In a nisl vulputate, molestie metus a, posuere arcu. Cras viverra aliquet cursus.
+
+Sed quis malesuada velit. Ut vel volutpat massa. Sed augue elit, ultricies ut tellus eget, sollicitudin fermentum turpis. Cras interdum eros ac ornare aliquam. Aliquam nisi elit, finibus hendrerit orci nec, tristique rhoncus orci. Etiam at lorem vitae ipsum sollicitudin consequat. Suspendisse ut tempus mi, mattis ultricies magna. Mauris fermentum lacus eget consequat hendrerit. Cras fermentum aliquet volutpat. Cras ante dui, gravida sed felis ac, posuere cursus augue. Proin vel enim rutrum, vehicula velit id, euismod velit. Quisque lectus arcu, cursus ac tincidunt vitae, porttitor vitae mi. Fusce eget tincidunt nibh.
+
+Integer et malesuada massa. Cras ultricies, nibh vel iaculis fermentum, neque augue ultricies mauris, vel feugiat velit dui non dui. Morbi a finibus urna. Fusce tempor pulvinar eros, eu finibus ipsum. Mauris vel ipsum a turpis ultricies posuere. Fusce non leo porta, blandit lectus et, euismod odio. Nam maximus ligula id ante malesuada, at eleifend dolor ultricies. Duis justo ipsum, viverra at luctus vitae, vulputate quis libero. Etiam volutpat lacus sit amet orci ultrices rutrum. Proin dignissim ullamcorper est at suscipit.
+
+Aliquam porttitor lorem posuere quam mattis sagittis. Proin ut augue rutrum magna dignissim luctus. Vestibulum vulputate consequat finibus. Nam in magna tincidunt, facilisis arcu eget, tristique erat. Duis blandit eros eget tellus feugiat, nec ornare metus tristique. Donec in ante ac lacus lacinia dapibus vel ut enim. Etiam vitae semper erat. Donec fermentum, ex vitae sollicitudin pellentesque, odio lectus dapibus justo, non ornare ligula purus in eros. Proin ac neque eu quam egestas faucibus. Proin eget ultricies enim, a dapibus massa. Maecenas malesuada odio vitae leo dapibus lobortis. Integer at finibus nisi.</h1>
     </>
   );
 }
